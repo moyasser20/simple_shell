f883ac0c16187761b543fab9034324236a9d8286
omarbdw <omarbdw@yahoo.com>
2023-10-11 12:23:30 +0300

    handling EOF, looping and memory leaks

--- shell.c
+++ shell.c
@@ -1,9 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
+#include "header.h"
+
 /**
  * main - prints prompt and waits for user to enter command
  * Return: 0 on success, -1 on failure
@@ -17,119 +13,168 @@ int main(void)
     int count = 1;
     for (; count > 0; count++)
     {
-        pid_t pid;
+        pid_t pid = 0;
         int status = 2;
         char *cmd = NULL;
         char *cmdPath = NULL;
         char binPath[] = "/usr/bin/";
         char *argVec[] = {NULL};
         char *envVec[] = {""};
-        char *cmd_copy;
+        char *cmd_copy = NULL;
         char *token = NULL;
         char *delim = " \n";
         int argc = 0;
         char **argv = NULL;
         size_t n = 0;
         int i = 0;
+        char *cmdPath_copy = NULL;
+        int cmd_len = 0;
+        int comp = 10;
 
         // print prompt
         printf("#cisfun$ ");
+        // printf("Flag Test: before getline()\n"); //////////////////////////////////////////////
+
         // get command from user
-        if (getline(&cmd, &n, stdin) == -1)
+        cmd_len = getline(&cmd, &n, stdin);
+        // check if getline failed
+        if (cmd_len == -1)
         {
             perror("getline");
             return (-1);
         }
-        // duplicate cmd
-        cmd_copy = strdup(cmd);
-        // check if cmd_copy is NULL
-        if (cmd_copy == NULL)
+        // check if getline failed
+        if (cmd_len > 1)
         {
-            perror("strdup");
-            return (-1);
-        }
-        // tokenize cmd
-        token = strtok(cmd, delim);
-        // count number of tokens in cmd
-        while (token)
-        {
-            token = strtok(NULL, delim);
-            argc++;
-        }
-        // allocate memory for argv
-        argv = malloc(sizeof(char *) * (argc+1));
-        // check if malloc failed
-        if (argv == NULL)
-        {
-            perror("malloc");
-            return (-1);
-        }
-        // tokenize cmd_copy
-        token = strtok(cmd_copy, delim);
-        // store tokens in argv
-        while (token)
-        {
-            argv[i] = token;
-            token = strtok(NULL, delim);
-            i++;
-        }
-        // set last element of argv to NULL
-        argv[i] = NULL;
-        // compare argv[0] to exit
-        int cmp = strcmp(argv[0], "exit");
-        if (cmp == 0)
-        {
-            exit(0);
-        }
-        else if (cmp != 0)
-        {
-            // fork process
-            pid = fork();
-            if (pid == 0)
+            // printf("Flag Test: after cmd\n"); //////////////////////////////////////////////
+
+            // duplicate cmd
+            cmd_copy = strdup(cmd);
+            // check if cmd_copy is NULL
+            if (cmd_copy == NULL)
             {
-                // allocate memory for cmdPath
-                cmdPath = malloc(sizeof(char) * (strlen(binPath) + strlen(argv[0]) + 1));
-                // check if malloc failed
-                if (cmdPath == NULL)
-                {
-                    perror("malloc");
-                    return (-1);
-                }
-                // concatenate binPath and argv[0]
-                cmdPath = strcat(binPath, argv[0]);
-                // duplicate cmdPath
-                char *cmdPath_copy = strdup(cmdPath);
-                // copy argv into argVec
-                for (i = 0; i < argc; i++)
-                {
-                    argVec[i] = argv[i];
-                }
-                // set last element of argVec to NULL
-                argVec[i] = NULL;
-                // execute command
-                if (execve(cmdPath_copy, argVec, envVec) == -1)
-                {
-                    if (argVec[0] != "NULL")
-                        printf("No such file or directory\n");
-                }
-            }
-            // check if fork failed
-            else if (pid == -1)
-            {
-                perror("fork");
+                perror("strdup");
                 return (-1);
             }
-            // parent process
-            else
+            // tokenize cmd
+            token = strtok(cmd, delim);
+            // count number of tokens in cmd
+            while (token)
             {
-                // wait for child process to terminate
-                wait(&status);
-                // free memory
-                free(cmd);
+                token = strtok(NULL, delim);
+                argc++;
+            }
+            // allocate memory for argv
+            argv = malloc(sizeof(char *) * (argc + 1));
+            // check if malloc failed
+            if (argv == NULL)
+            {
+                perror("malloc");
+                return (-1);
+            }
+            // tokenize cmd_copy
+            token = strtok(cmd_copy, delim);
+            // store tokens in argv
+            while (token)
+            {
+                argv[i] = token;
+                token = strtok(NULL, delim);
+                i++;
+            }
+            // set last element of argv to NULL
+            argv[i] = NULL;
+            // printf("Flag Test: after argv\n"); //////////////////////////////////////////////
+
+            // compare argv[0] to exit
+            comp = strcmp(argv[0], "exit");
+            // printf("comp = %d\n", comp);       //////////////////////////////////////////////
+            // printf("argv[0] = %s\n", argv[0]); //////////////////////////////////////////////
+            if (comp == 0)
+            {
+                exit(0);
+                // printf("Flag test: performed exit"); //////////////////////////////////////////////
+                //  free memory
                 free(cmd_copy);
                 free(argv);
                 free(cmdPath);
-                // exit
+                free(cmdPath_copy);
+            }
+            else if (comp != 0)
+            {
+                // fork process
+                    // printf("Flag Test: before fork\n"); //////////////////////////////////////////////
+
+                pid = fork();
+
+                if (pid == 0)
+                {
+                    // allocate memory for cmdPath
+                    // printf("Flag Test: child process\n"); //////////////////////////////////////////////
+                    cmdPath = malloc(sizeof(char) * (strlen(binPath) + strlen(argv[0]) + 1));
+                    // check if malloc failed
+                    if (cmdPath == NULL)
+                    {
+                        perror("malloc");
+                        return (-1);
+                    }
+                    // get path of command
+                    cmdPath = _getpath(binPath, argv);
+                    // printf("Flag Test: after getting path\n"); //////////////////////////////////////////////
+
+                    // duplicate cmdPath
+                    cmdPath_copy = strdup(cmdPath);
+                    // check if cmdPath_copy is NULL
+                    if (cmdPath_copy == NULL)
+                    {
+                        perror("strdup");
+                        return (-1);
+                    }
+                    // copy argv into argVec
+                    for (i = 0; i < argc; i++)
+                    {
+                        argVec[i] = argv[i];
+                    }
+                    // set last element of argVec to NULL
+                    argVec[i] = NULL;
+                    // printf("Flag Test: before execve\n"); //////////////////////////////////////////////
+
+                    // execute command
+                    if (execve(cmdPath_copy, argVec, envVec) == -1)
+                    {
+                        if (argVec[0] != "NULL")
+                        {
+                            perror("execve");
+                            // printf("Bad Command\n");
+                        }
+                        exit(0);
+                    }
+
+                    // printf("Flag Test: after execve\n"); //////////////////////////////////////////////
+                }
+
+                // parent process
+                else if (pid > 0)
+                {
+                    // printf("Flag Test: just started parent\n"); //////////////////////////////////////////////
+
+                    // wait for child process to terminate
+                    wait(NULL);
+                    //   printf("Flag Test: back to parent process\n"); //////////////////////////////////////////////
+                }
+                // check if fork failed
+                else
+                {
+                    perror("fork");
+                    exit(1);
+                }
+                // free memory
+                free(cmd_copy);
+                free(argv);
+                free(cmdPath);
+                free(cmdPath_copy);
+
+
+                // printf("Flag Test: memory free is ok\n"); //////////////////////////////////////////////
             }
         }
     }
--- shell.h
+++ shell.h
@@ -2,7 +2,7 @@
  * @file shell.h
  * @brief Header file for shell.c
  * @details Contains the function prototypes and global variables for shell.c
-*/
+ */
 #ifndef SHELL_H
 #define SHELL_H
 
@@ -16,22 +16,43 @@
 
 // Defines
 
-
-
 // Prototypes
+char *_getpath(char *binPath, char **argv);
 
+// Function to return the path
+
+/**
+ * @brief Function to return the path
+ * @details This function returns the path of the command to be executed
+ * @param binPath The path of the bin directory
+ * @param argv The command to be executed
+ * @return The path of the command
+ */
+char *_getpath(char *binPath, char **argv)
+{
+    char *cmdPath = NULL;
+
+    //allocate memory for cmdPath
+    cmdPath = malloc(sizeof(char) * (strlen(binPath) + strlen(argv[0]) + 1));
+    // check if malloc failed
+    if (cmdPath == NULL)
+    {
+        perror("malloc");
+    }
+    // concatenate binPath and argv[0]
+    cmdPath = strcat(binPath, argv[0]);
+    // add null terminator to cmdPath
+    cmdPath[strlen(binPath) + strlen(argv[0])] = '\0';
+    return (cmdPath);
+    free(cmdPath);
+}
 
 // Function to print the prompt
 
-
 // Function to read the command line
-
 
 // Function to parse the command line
 
-
 // Function to execute the command
-
-
 
 #endif
